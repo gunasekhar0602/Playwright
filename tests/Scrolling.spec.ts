4718bcd542712c2911ceb25f4bc96835c3cfcd4e
gunasekhar0602 <gunabandla2000@gmail.com>
2026-04-15 15:49:12 +0530

    Add scrolling examples to Scrolling.spec.ts
    Added examples for scrolling to the bottom of the page and getting the current page height.

--- tests/Scrolling.spec.ts
+++ tests/Scrolling.spec.ts
@@ -17,3 +17,23 @@ test.only ("scrolling to inside the dropdown", async ({page})=>
 	await page. locator('#dropdown divinth-child(100)'). click();
 	await page.waitfortimeout(5000);
 })
+
+
+
+
+//Example 1: Scroll to the bottom of the page
+ 
+await page.evaluate(() =>
+{
+	window.scrollTo(0, document.body.scrollHeight);
+});
+ 
+ 
+ 
+//Example 2: Get the current page height (Total scrollable height of the page)
+ 
+const currentHeight = await page.evaluate(() =>
+{
+	 return document.body.scrollHeight;
+ });
+
