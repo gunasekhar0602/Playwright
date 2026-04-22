6be32fb25cce42dc381ccc461835df8f874b4f85
guna <gunabandla2000@gmail.com>
2026-04-22 16:25:22 +0530

    Mouseactions

--- tests/Mouseactions.spec.ts
+++ tests/Mouseactions.spec.ts
@@ -1,69 +1,94 @@
 //1. Mouse Hover – hover()
 
-//Purpose:To simulate a mouse hover over an element (useful for revealing dropdowns or tooltips).
- 
-//Syntax:await page.locator('selector').hover();
- 
-//Example:await page.locator('#menu').hover();
+//Purpose:
+// To simulate a mouse hover over an element (useful for revealing dropdowns or tooltips).
 
-test ("Mouse Hover", async ({page}) =>
+//Syntax:
+// await page.locator('selector').hover();
+
+//Example:
+// await page.locator('#menu').hover();
+
+import{test,expect,Locator} from'@playwright/test'
+test("Mouse Hover", async ({page}) =>
 {
+    // Navigating to page
 	await page.goto('https://testautomationpractice.blogspot.com/');
-    // after capturing the element,we have to hover on the element
-	const pointmebtn:Locator= await page.getByText(‘Point Me’);
-	await pointmebtn.hover();
-	await page.waitForTimeout (5000);
 
-	const laptopoption=page.locator(‘.dropdown-content a:nth-child(2)’);
-  
-// (Dropdown locator with tagname and element in the dropdown)
+    // after capturing the element,we have to hover on the element
+	const pointmebtn:Locator=page.getByText('Point Me');
+	await pointmebtn.hover();
+	await page.waitForTimeout(3000);
+
+    // Locating the one of the element after hover on the element
+    const laptopoption=page.locator("//div[@class='dropdown-content']//a[2]");
+	//const laptopoption=page.locator('.dropdown-content a:nth-child(2)');
+
+    // hover on Locting element
 	await laptopoption.hover();
 	await page.waitForTimeout (5000);
-});
+})
 
 
 
 //2. Right Click – click({ button: 'right' })
- 
-//Purpose: To simulate a right-click on an element.
- 
-//Syntax: await page.locator('selector').click({ button: 'right' });
- 
-//Example: await page.locator('#file-icon').click({ button: 'right' });
+
+//Purpose: 
+// To simulate a right-click on an element.
+
+//Syntax:
+// await page.locator('selector').click({ button: 'right' });
+
+//Example: 
+// await page.locator('#file-icon').click({ button: 'right' });
 
 test("Right click", async ({page}) =>
 {
-page.goto('https://swisnl.github.io/jQuery-contextMenu/demo.html');
-const button: Locator=await page. locator('span. context-menu-one');
-await button click({button: 'right'});
-// This will perform the right click action
+    // Navigate to the page
+    page.goto('https://swisnl.github.io/jQuery-contextMenu/demo.html');
 
-await page. waitForTimeout (5000);
+    // Locating the button element
+    //const rightbutton:Locator= page.locator('span.context-menu-one');
+    const rightbutton1:Locator= page.locator("//span[text()='right click me']");
 
-});
+    // Perming right click on the element
+    await rightbutton1.click({button:'right'});
+    // This will perform the right click action
+
+    await page. waitForTimeout (5000);
+})
 
 
 
 
 
 //3.Double Click – dblclick()
- 
-//Purpose:To simulate a double-click on an element.
- 
-//Syntax: await page.locator('selector').dblclick();
- 
-//Example:await page.locator('#editable-text').dblclick();
- 
- 
-test ("Double click", async ({page})=>
+
+//Purpose:
+// To simulate a double-click on an element.
+
+//Syntax:
+// await page.locator('selector').dblclick();
+
+//Example:
+// await page.locator('#editable-text').dblclick();
+
+
+test("Double click", async ({page})=>
 {
-	await page, goto('https://testautomationpractice.blogspot.com/');
-	const doubleclick: Locator=page.getByText("Copy Text');
-// performs double click
+    // Navigate to page
+	await page.goto('https://testautomationpractice.blogspot.com/');
+
+    // Locate the element
+	const doubleclick: Locator=page.getByText('Copy Text');
+
+    // performs double click     - dblclick() - Double click action
     await doubleclick.dblclick();
 
+    // Verifying the second box after the double click
+    // Locating the second box and putting assertion
     const field2: Locator=page.locator('#field2');
-	await expect (field2). toHaveValue("Hello World!");
+	await expect(field2).toHaveValue("Hello World!");
 	await page.waitForTimeout(5000);
 });
 
@@ -72,82 +97,42 @@ test ("Double click", async ({page})=>
 
 //4. Drag and Drop – dragTo()
  
-//Purpose:To drag an element and drop it to another target.
+//Purpose:
+// To drag an element and drop it to another target.
  
-//Syntax:await page.locator('source-selector').dragTo(page.locator('target-selector'));
+//Syntax:
+// await page.locator('source-selector').dragTo(page.locator('target-selector'));
 
-test. only("Drag and drop", async ({page}) =>
+test.only("Drag and drop", async ({page}) =>
 {
+    // Navigating to page
     await page.goto('https://testautomationpractice.blogspot.com/');
 
-    const intialele: Locator=page. locator ("div#draggable");
+    // Here we need to find the source element and target element
+    // Locating initial element
+    const intialele: Locator=page.locator ("div#draggable");
+
+    // Locating target element
     const targetele: Locator=page.locator ('div #droppable');
 
-// Here we need to find the source element and target element
+    // Here we need to find the source element and target element
 
 //approach 1) - mouse hover and drag manually
     await intialele.hover();
-    await page. mouse. down (); 
+    await page.mouse.down (); 
     await targetele.hover(); 
     await page.mouse.up();
-
     await page.waitForTimeout(5000);
 
 // approach 2)- drag and drop method
     await intialele.dragTo(targetele);
-    await page.waitForTimeout(5000)
+    await page.waitForTimeout(5000); 
 
+    //Example:
+
+    //const source = page.locator('#drag-item');
+    //const target = page.locator('#drop-zone');
+    //await source.dragTo(target);
 })
- 
-//Example:
- 
-    const source = page.locator('#drag-item');
-    const target = page.locator('#drop-zone');
-    await source.dragTo(target);
- 
- 
-
-
-
-
-
-//5. Mouse Down – page.mouse.down()
- 
-//Purpose: To press and hold the mouse button down (without releasing it).
- 
-//Syntax: await page.mouse.down();
- 
-Example:
- 
-await page.mouse.move(100, 200); // move to a position
-await page.mouse.down(); // press mouse button
-
-
-
-
-
-//6. Mouse Up – page.mouse.up()
-
-//Purpose:To release the mouse button (after a mouse down).
- 
-//Syntax: await page.mouse.up();
- 
-Example:
-
-await page.mouse.up(); // usually follows a mouse.down()
- 
- 
- 
-//7. Mouse Move – page.mouse.move(x, y)
- 
-//Purpose:To move the mouse to a specific position on the screen.
- 
-// Syntax: await page.mouse.move(x, y);
- 
-Example:
- 
-await page.mouse.move(150, 300);
-
-
 
 
