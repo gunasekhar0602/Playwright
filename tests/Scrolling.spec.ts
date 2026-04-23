2b1886ae671dd6e06f268574656edd152e1705a4
gunasekhar0602 <gunabandla2000@gmail.com>
2026-04-23 12:41:52 +0530

    Refactor comments and improve readability in tests

--- tests/Scrolling.spec.ts
+++ tests/Scrolling.spec.ts
@@ -57,15 +57,12 @@ test.only("Infinite Scrolling", async({page})=>
 {
     test.slow(); // This will triple the default timeout of playwright
     await page.goto("https://www.booksbykilo.in/new-books?pricerange=210to500");
-    
-    // Here we need to execute Java Script statement // window - represeneting our application
-    // In the window we need to scroll to from 0 (Here 0 is starting point)
-    // document.body.scrollHeight - reprenting the end of the page
-    // This the statement we need to execute multiple times till it reaches to end of the page
-    // window.scrollTo(0,document.body.scrollHeight) - should mention inside evaluate function
-
-    // We need to compare the current previousHeight and currentHeight
-    let previousHeight=0;
+// Here we need to execute Java Script statement // window - represeneting our application
+// In the window we need to scroll to from 0 (Here 0 is starting point)
+// document.body.scrollHeight - reprenting the end of the page
+// This the statement we need to execute multiple times till it reaches to end of the page
+// window.scrollTo(0,document.body.scrollHeight) - should mention inside evaluate function
+    let previousHeight=0; // We need to compare the current previousHeight and currentHeight
     let bookfound=false;   // Verifying the bookm is there OR not.
     while(true)
     {
@@ -77,30 +74,22 @@ test.only("Infinite Scrolling", async({page})=>
             expect(bookfound).toBeTruthy();
             break; // If book is present means break the loop.
         }
-
-        // evaluate will take an arrow function as Parameter. // Scrolling the page
-        
+// evaluate will take an arrow function as Parameter. // Scrolling the page
         await page.evaluate(()=>{
             window.scrollTo(0,document.body.scrollHeight)
         })
-        // wait for new content to load
-        await page.waitForTimeout(2000);
-
-        // evaluate will take an arrow function as Parameter
-        // Capture current height of the page
+        await page.waitForTimeout(2000); // wait for new content to load
+// evaluate will take an arrow function as Parameter. // Capture current height of the page
         const currentHeight=await page.evaluate(()=>{
             return document.body.scrollHeight;
         })
         console.log("Previous height:",previousHeight)
         console.log("current height:",currentHeight)
-
-        // if current height === previous height then break
         if(currentHeight===previousHeight)
         {
-            break;
+            break;       // if current height === previous height then break
         }
-        // else assign previous height = current height
-        previousHeight=currentHeight;
+        previousHeight=currentHeight;     // else assign previous height = current height
     }
     console.log('We reached end of the page')
     
