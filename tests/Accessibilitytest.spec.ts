1c31b089f4fd896a47acf4801a881ca765d17cbe
gunasekhar0602 <gunabandla2000@gmail.com>
2026-04-25 04:13:14 +0530

    Update Accessibilitytest.spec.ts

--- tests/Accessibilitytest.spec.ts
+++ tests/Accessibilitytest.spec.ts
@@ -1,1 +1,11 @@
+import { test, expect } from '@playwright/test';
 
+
+import Axebuilder from '@axe-core/playwright';
+test ("accessibility test", async ({page}) =>
+await page.goto('https://demowebshop.tricentis.com/')
+//1) Scanning detect all types of WAG violations.
+const accessibilityScanResults=await new AxeBuilder({page}) . analyze() ;
+console.log( "Number of violations:====>" , accessibilityScanesults.violations.length);
+//expect(accessibilityScanResults.violations).toEqual([]);
+expect (accessibilityScanResults.violations.length).toEqual(0);
