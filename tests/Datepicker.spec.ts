75bafb191575a864b8fb1b93728f37b05fe7380a
gunasekhar0602 <gunabandla2000@gmail.com>
2026-08-02 22:25:59 +0530

    Update Datepicker.spec.ts

--- tests/Datepicker.spec.ts
+++ tests/Datepicker.spec.ts
@@ -27,11 +27,13 @@ async function selectDate(tYear:string,tMonth:string,tdate:string,page:Page,isFu
 
 // locating and Capturing all dates in array formate
     const allDates=await page.locator(".ui-datepicker-calendar td").all();
+    
 // From for loop we will extracgt the inner text of all the date elements
     for(let dt of allDates)
     {
     // create a variable to capture the inner text of date elements
         const datetext=await dt.innerText();
+        
     // If both datetext and date are same means click on the dt and break the loop
         if(datetext===tdate)
         {
@@ -45,12 +47,13 @@ test("Query datepicker", async({page})=>
     await page.goto("https://testautomationpractice.blogspot.com/"); // Navigate to page
     const dateInput:Locator=page.locator('#datepicker'); // Locate the Dataepicker element
     expect (dateInput).toBeVisible();
-await dateInput.click();    // opens the date picker
+    await dateInput.click();    // opens the date picker
+    
 //select target date 
 const year='2026'; const month='June'; const date='15';
 selectDate(year,month,date,page,true) // calling function
-//const expectedDate='06/15/2026';
-//await expect(dateInput).toHaveValue(expectedDate) // assertion
+const expectedDate='06/15/2026';
+await expect(dateInput).toHaveValue(expectedDate) // assertion
 })
 
 
